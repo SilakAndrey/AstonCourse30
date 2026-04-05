a41d54a2f634f525ca3f4f554e26844bb59a7998
SilakAndrey <silakandreyjob@gmail.com>
2026-04-05 15:39:13 +0300

    add new branch with lessonOne

--- src/main/java/org/example/LessonOne.java
+++ src/main/java/org/example/LessonOne.java
@@ -13,10 +13,10 @@ public class LessonOne {
         task8("Не ставьте 59 баллов ", 5);
         System.out.println("\n\n" + task9(1900) + "\n");
         task10();
-        //task11();
-        //task12();
-        //task13();
-        //task14();
+        task11();
+        task12();
+        task13();
+        task14(10, 59);
     }
 
     public static void printThreeWords(){
@@ -100,12 +100,74 @@ public class LessonOne {
         int[] a = new int[5];
         a[0] = 1; a[1] = 0; a[2] = 1; a[3] = 0; a[4] = 1;
         for (int i = 0; i < 5; i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println("");
+        for (int i = 0; i < 5; i++){
             if(a[i] == 1){
                 a[i] = 0;
             }
             else {a[i] = 1;
             }
         }
+         for (int i = 0; i < 5; i++){
+             System.out.print(a[i] + " ");
+         }
+         System.out.println("\n");
      }
 
+    public static void task11(){
+        int[] a = new int[100];
+        for (int i = 0; i < 100; i++){
+            a[i] = i + 1;
+        }
+        for (int i = 0; i < 100; i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println("\n");
+    }
+
+    public static void task12(){
+        int[] a = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < 12; i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println("");
+        for (int i = 0; i < 12; i++){
+            if(a[i] < 6){
+                a[i] *= 2;
+            }
+        }
+        for (int i = 0; i < 12; i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println("\n");
+    }
+
+    public static void task13(){
+        int[][] a = new int[5][5];
+        for (int i = 0; i < 5; i++){
+            for (int j1 = 0, j2 = 4; j1 < 5; j1++, j2--){
+                if (j1 == i){
+                    a[i][j1] = 1; a[i][j2] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("\n");
+    }
+
+    public static int[] task14(int len, int initialValue){
+        int[] a = new int[len];
+            for (int i = 0; i < len; i++){
+                a[i] = initialValue;
+                System.out.print(a[i] + " ");
+            }
+        return a;
+    }
 }
