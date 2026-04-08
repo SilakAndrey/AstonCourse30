d51df6d554f89977e70c3faf3bc0f920d325ae49
SilakAndrey <silakandreyjob@gmail.com>
2026-04-08 18:32:10 +0300

    add new branch with Товар

--- src/main/java/org/example/Товар.java
+++ src/main/java/org/example/Товар.java
@@ -2,16 +2,24 @@ package org.example;
 
 public class Товар {
     public static void main(String[] args) {
-        Товар a = new Товар("Часы", "19.09.2009", "Сунь Цзынь", "Китай", 65000, "не забронирован");
+        Товар a = new Товар("Часы", "19.09.2009", "Сунь Цзынь", "Китай", 65000, false);
         a.info();
+
+        Товар[] productsArray = new Товар[5];
+        productsArray[0] = new Товар("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 5599, true);
+        productsArray[1] = new Товар("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 5599, true);
+        productsArray[2] = new Товар("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 5599, true);
+        productsArray[3] = new Товар("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 5599, true);
+        productsArray[4] = new Товар("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 5599, true);
+
     }
     private String название;
     private String датаПроизводства;
     private String производитель;
     private String странаПроисхождения;
     private int цена;
-    private String состояниеБронированияПокупателем;
-    public Товар (String название, String датаПроизводства, String производитель, String странаПроисхождения, int цена, String состояниеБронированияПокупателем){
+    private boolean состояниеБронированияПокупателем;
+    public Товар (String название, String датаПроизводства, String производитель, String странаПроисхождения, int цена, boolean состояниеБронированияПокупателем){
         this.название = название;
         this.датаПроизводства = датаПроизводства;
         this.производитель = производитель;
@@ -25,7 +33,7 @@ public class Товар {
         System.out.println("Производитель: " + производитель);
         System.out.println("Страна происхождения: " + странаПроисхождения);
         System.out.println("Цена: " + цена);
-        System.out.println("Состояние бронирования покупателем: " + состояниеБронированияПокупателем);
+        System.out.println("Состояние бронирования покупателем: " + (состояниеБронированияПокупателем ? "Забронирован" : "Не забронирован"));
     }
 }
 
